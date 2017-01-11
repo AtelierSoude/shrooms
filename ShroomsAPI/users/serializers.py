26c0fc1af51f2b2785eb6c02b1b960a0fbb47f94
Louis <ls.duchemin@gmail.com>
2017-01-11 13:40:45 +0100

    EER diagram update

--- ShroomsAPI/users/serializers.py
+++ ShroomsAPI/users/serializers.py
@@ -1,8 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from rest_framework import permissions
-from rest_framework.serializers import (HyperlinkedIdentityField,
-                                        HyperlinkedModelSerializer)
+from rest_framework.serializers import HyperlinkedModelSerializer
 
 """
 FIELDS
--- ShroomsAPI/users/views.py
+++ ShroomsAPI/users/views.py
@@ -1,8 +1,7 @@
-
-
-from actstream import action
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
+
+from actstream import action
 from rest_framework import generics, permissions, status, viewsets
 from rest_framework.response import Response
 
@@ -21,6 +20,7 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAdminUser,)
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -29,8 +29,9 @@ class UserViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         return serializer.save()
+
 
 class PermissionViewSet(viewsets.ModelViewSet):
     """
