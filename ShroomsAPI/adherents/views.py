3a7aca57ff5021547e7ad0fe7be4e62b341ad00d
louis <ls.duchemin@gmail.com>
2017-02-13 16:16:37 +0100

    Adherent viewset

--- ShroomsAPI/adherents/views.py
+++ ShroomsAPI/adherents/views.py
@@ -1,22 +1,45 @@
+from adherents.models import Adherent, Subscription, SubscriptionType
+from adherents.serializers import (AdherentSerializer, AdherentShortSerializer,
+                                   SubscribeSerializer, SubscriptionSerializer,
+                                   SubscriptionTypeSerializer)
+from django.contrib.auth import get_user_model, models
 from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth import models
+from dry_rest_permissions.generics import DRYPermissions
+from profiles.views import ProfileViewSet
+from profiles.mixins import AltSerializerMixin
 from rest_framework import generics, permissions, status, views, viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework_extensions.mixins import NestedViewSetMixin
-from adherents.models import Adherent, Subscription, SubscriptionType
-from adherents.serializers import (
-    SubscribeSerializer, SubscriptionSerializer,
-    SubscriptionTypeSerializer)
-from django.contrib.auth import get_user_model
 
 USERNAME_FIELD = get_user_model().USERNAME_FIELD
 
 """
 API views
 """
+
+
+class AdherentViewSet(ProfileViewSet, AltSerializerMixin):
+    """
+    Adherent viewset with alternative serializer to use depending
+    on auth user.
+    Shows only currently subscribed Profiles
+    """
+    queryset = Adherent.objects.active()
+    permission_classes = (DRYPermissions,)
+    serializer_class = AdherentShortSerializer
+    alt_serializer_class = AdherentSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        "Custom retrieve method allowing alternative serializer"
+        instance = self.get_object()
+        if instance.user == request.user or request.user.is_staff or request.user.is_superuser:
+            serializer = self.get_alt_serializer(instance)
+        else:
+            serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 
 class SubscribeView(generics.CreateAPIView):
@@ -36,7 +59,7 @@ class SubscriptionTypeViewSet(viewsets.ModelViewSet):
     """
     Subscription types viewset
     """
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (DRYPermissions,)
     queryset = SubscriptionType.objects.all()
     serializer_class = SubscriptionTypeSerializer
 
@@ -45,12 +68,13 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
     """
     Subscription viewset
     """
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (DRYPermissions,)
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
 
     @list_route()
     def active(self, request):
+        "List currently active subscriptions"
         active_sub = Subscription.objects.active()
         page = self.paginate_queryset(active_sub)
         if page is not None:
